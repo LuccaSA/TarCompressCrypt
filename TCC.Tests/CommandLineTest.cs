362ebaff75562a7c2b87b9da7ddb534a67dd0d06
Raphaël DUCOM <raphael.ducom@gmail.com>
2018-10-06 00:01:56 +0200

    adds compress test

--- TCC.Tests/CommandLineTest.cs
+++ TCC.Tests/CommandLineTest.cs
@@ -10,5 +10,11 @@ namespace TCC.Tests
         {
             await Program.Main("benchmark -e false -a Zstd -r 1 -c Ascii -n 1 -s 4096 -t 1".Split(" "));
         }
+
+        [Fact]
+        public async Task CompressCall()
+        {
+            await Program.Main("compress .\\TCC.dll -a Zstd -r 1 -f -p 1234 -t 1 -o .\\test".Split(" "));
+        }
     }
 }
