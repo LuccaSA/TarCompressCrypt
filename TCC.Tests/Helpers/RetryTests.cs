c90db8e6e70f611e1d2bac5c1be43564e478a6fa
Raphaël DUCOM <raphael.ducom@gmail.com>
2021-03-16 09:22:05 +0100

    adds one more retry step

--- TCC.Tests/Helpers/RetryTests.cs
+++ TCC.Tests/Helpers/RetryTests.cs
@@ -17,16 +17,19 @@ namespace TCC.Tests.Helpers
             var ok2 = Retry.CanRetryIn(out TimeSpan retry2, ref retry);
             var ok3 = Retry.CanRetryIn(out TimeSpan retry3, ref retry);
             var ok4 = Retry.CanRetryIn(out TimeSpan retry4, ref retry);
+            var ok5 = Retry.CanRetryIn(out TimeSpan retry5, ref retry);
 
             Assert.True(ok1);
             Assert.True(ok2);
             Assert.True(ok3);
-            Assert.False(ok4);
+            Assert.True(ok4);
+            Assert.False(ok5);
 
             Assert.True(retry1 >= TimeSpan.FromSeconds(1) && retry1 <= TimeSpan.FromSeconds(2));
             Assert.True(retry2 >= TimeSpan.FromSeconds(3) && retry2 <= TimeSpan.FromSeconds(4));
             Assert.True(retry3 >= TimeSpan.FromSeconds(14) && retry3 <= TimeSpan.FromSeconds(15));
-            Assert.Equal(TimeSpan.Zero,retry4);
+            Assert.True(retry4 >= TimeSpan.FromSeconds(42) && retry4 <= TimeSpan.FromSeconds(43));
+            Assert.Equal(TimeSpan.Zero,retry5);
             ;
         }
     }
